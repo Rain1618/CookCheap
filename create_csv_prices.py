240b781a183634418d98c5ccf2f30219a52c046e
gülce <115143411+gcc4p@users.noreply.github.com>
2023-11-19 01:12:55 -0500

    new repo yay

---
+++ create_csv_prices.py
@@ -0,0 +1,81 @@
+import csv
+#from calculate_costs import provigo_info
+from instacart_web_scraping import instacart_info
+
+ing1 = ['canola oil', 'olive oil', 'balsamic vinegar', 'white vinegar']
+ing2 = ['rice vinegar', 'ketchup', 'mayonnaise','dijon mustard']
+ing3 = ['soy sauce', 'chili paste', 'hot sauce', 'cayenne pepper']
+ing4 = ['cumin', 'coriander', 'oregano', 'paprika']
+ing5 = ['rosemary', 'thyme leaves', 'cinnamon', 'cloves']
+ing6 = ['allspice', 'ginger', 'nutmeg','chili powder']
+ing7 = ['curry powder', 'vanilla extract', 'canned black beans', 'canned chickpeas']
+ing8 = ['olives', 'peanut butter', 'canned tomatoes', 'tomato paste']
+ing9 = ['salsa', 'canned tuna fish', 'couscous', 'regular pasta']
+ing10 = ['rice', 'baking powder', 'baking soda', 'brown sugar'] 
+ing11 = ['cornstarch', 'all-purpose flour', 'granulated sugar', 'honey']
+ing12 = ['butter', 'cheddar cheese', 'feta cheese', 'parmesan cheese']
+ing13 = ['mozzarella cheese', 'eggs', 'milk', 'plain yogurt'] 
+ing14 = ['blackberries', 'blueberries', 'peaches', 'strawberries']
+ing15 = ['broccoli', 'corn', 'peas', 'spinach']
+ing16 = ['garlic', 'onions', 'potatoes', 'dried raisins']
+ing17 = ['dried apples', 'dried apricots', 'almonds', 'peanuts'] 
+ing18 = ['sunflower seeds', 'chicken', 'salt', 'pepper']
+ing19 = ['pork', 'beef', 'orange', 'turkey']
+ing20 = ['bacon', 'coconut', 'mushrooms', 'beets']
+ing21 = ['fennel seeds', 'lamb', 'shrimp', 'white bread']
+ing22 = ['baguette', 'wholegrain bread', 'sourdough bread', 'naan bread']
+ing23 = ['avocado oil', 'sesame oil', 'sunflower oil', 'apple cider vinegar']
+ing24 = ['yellow mustard', 'basil', 'dill', 'sage']
+ing25 = ['tarragon', 'cardamom', 'turmeric', 'lentils']
+ing26 = ['quinoa', 'barley', 'penne pasta', 'fusilli pasta']
+ing27 = ['farfalle pasta', 'spaghetti', 'maple syrup', 'gouda cheese']
+ing28 = ['swiss cheese', 'goat cheese', 'walnuts', 'pecans']
+ing29 = ['chia seeds', 'flaxseeds', 'cranberries', 'dates']
+ing30 = ['salmon', 'lamb chops', 'pita bread', 'almond milk']
+ing31 = ['coconut milk', 'bell peppers', 'zucchini', 'asparagus']
+ing32 = ['mango', 'pineapple', 'kiwi', 'pesto sauce']
+ing33 = ['hummus', 'carrots', 'celery', 'leeks']
+ing34 = ['artichokes', 'brussels sprouts', 'cauliflower', 'eggplant']
+ing35 = ['kale', 'radish', 'cabbage', 'green beans']
+ing36 = ['cucumber', 'apple', 'pear', 'pickle']
+ing37 = ['chocolate', 'candy', 'frozen pizza', 'banana']
+
+
+#Give big list of common ingredients to provigo_info
+data = instacart_info(ing7, "metro")
+
+def make_csv(store_name, data):
+    csv_file = store_name+'.csv'
+
+    # Write the dictionary to a CSV file
+    with open(csv_file, 'w', newline='') as file:
+        writer = csv.writer(file)
+        
+        # Write the header
+        writer.writerow(['Ingredient', 'Price', 'Quantity', 'Unit Price', 'Unit', 'Url'])
+        
+        # Write the data
+        for ingredient, info in data.items():
+            writer.writerow([ingredient, info['Price'], info['Quantity'], info['UnitPrice'], info['Unit'], info['Url']])
+
+    print(f'CSV file "{csv_file}" created successfully.')
+    
+
+def append_to_csv(store_name, additional_data):
+    csv_file = store_name + '.csv'
+
+    # Open the CSV file in append mode
+    with open(csv_file, 'a', newline='') as file:
+        writer = csv.writer(file)
+
+        # Append the new data to the file
+        for ingredient, info in additional_data.items():
+            writer.writerow([ingredient, info['Price'], info['Quantity'], info['UnitPrice'], info['Unit'], info['Url']])
+
+    print(f'Data appended to CSV file "{csv_file}" successfully.')
+    
+
+#make_csv("metro", data)
+append_to_csv("metro", data)
+
+
---
+++ instacart_web_scraping.py
@@ -0,0 +1,146 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+#from destroy_popup import destroy_popup
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from csv import writer
+import os
+
+
+def get_info_instacart(url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    time.sleep(5)
+    html = driver.page_source
+    driver.quit()
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    name = soup.find("span", class_='e-17kpahp') 
+    
+    price = soup.find("span", class_ ='e-1ls9hv7')
+    if not price:
+        price = soup.find("span", class_ ='e-0')
+    
+    if price:
+        quantity = price.get_text(strip=True)
+        try:
+            quantity = quantity.split('/')[1]
+        except IndexError:
+            quantity = "per pack"
+    else:
+        quantity = 0
+        
+    unitprice = soup.find("div", class_ ='e-1og1nqr')
+    if unitprice:
+        unit = unitprice.get_text(strip=True)
+        try:
+            unit = unit.split('/')[1]
+        except IndexError:
+            unit = "per unit"
+    else:
+        unit = 0
+
+    # Extract the text content
+    if name:
+        name_text = name.get_text(strip=True)
+        print("Extracted Name:", name_text)
+    else:
+        name_text = "-1"
+        print("Name not found.")
+
+    if price:
+        price_text = price.get_text(strip=True)
+        try:
+            price_text = float(price_text.split('/')[0][1:])
+        except ValueError:
+            price_text = float(price_text.split()[0][1:])
+        print("Extracted Price:", price_text)
+    else:
+        price_text = "-1"
+        print("Price not found.")
+
+    if quantity:
+        quantity_text = quantity
+        print("Extracted Quantity:", quantity_text)
+    else:
+        quantity_text = "-1"
+        print("Quantity not found.")
+    
+    if unitprice:
+        unit_price = unitprice.get_text(strip=True)
+        try:
+            unit_price = float(unit_price.split('/')[0][1:])
+        except ValueError:
+            unit_price = float(unit_price.split()[0][1:])
+        print("Extracted Unit Price:", unit_price)
+    else:
+        unit_price = "null"
+        print("Unit price not found.")
+    
+    if unit:
+        print("Extracted Unit:", unit)
+    else:
+        unit = "null"
+        print("Unit not found.")
+        
+
+    print("Url: ", url)
+
+    info = dict(Price = price_text, Quantity = quantity_text, UnitPrice = unit_price, Unit = unit, Url = url)
+    print(info)
+    return name_text, info
+
+
+
+def get_url_instacart(name_produce, store):
+    '''(str, str) --> (str)
+    name_produce: name of food to search
+    store: metro, adonis, costco-canada, super-c, walmart-canada
+    Given the name of a product, output the url of the page containing said product
+    '''
+    search_url = f"https://www.instacart.ca/store/{store}/s?k={name_produce}"
+    driver = webdriver.Chrome()
+    driver.get(search_url)
+
+    # Wait for the page to load (adjust the wait time as needed)
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "e-1dlf43s"))
+    )
+    revealed = driver.find_element(By.CLASS_NAME, 'e-1dlf43s')
+
+    wait = WebDriverWait(driver, timeout=2)
+    wait.until(lambda d : revealed.is_displayed())
+
+    html = driver.page_source
+    # time.sleep(30) #Keeps browser open, debugging purposes
+    driver.quit()
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    link = soup.find("a", class_="e-1dlf43s")['href']
+    link = "https://www.instacart.ca"+link
+    print(link)
+    return link
+
+
+def instacart_info(ingredients, store):
+    '''(list of strings) --> dict
+    '''
+    instacart = {}
+    for ingredient in ingredients:
+        url = get_url_instacart(ingredient, store)
+        name, info = get_info_instacart(url)
+        #If we cannot find a produce, don't include in the dictionary
+        if name == -1:
+            continue
+        instacart.update({name: info})
+    
+    return instacart
+
+#instacart_info(['mango', 'pineapple', 'kiwi', 'pesto sauce'], 'provigo')
